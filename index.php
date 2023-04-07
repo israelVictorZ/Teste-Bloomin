eadf3b1a54fa7d28124b0a140f9ae0bc333ea29b
Israel_Victor <israel-victor@bol.com.br>
2023-04-07 05:05:22 -0300

    initial-commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// HEAD
+include('includes/head.php');
+
+// HEADER
+include('includes/components/header.php');
+
+// BANNER
+include('includes/components/banner.php');
+
+// PRODUCTS
+include('includes/components/products.php');
+
+// ABOUT
+include('includes/components/about.php');
+
+// CONTACT
+include('includes/components/contact.php');
+
+// FOOTER
+include('includes/components/footer.php');
+
+?>
